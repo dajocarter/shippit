7454f6a15b896d6d89096939e55aee92033c118f
David Carter <dajocarter@gmail.com>
2018-06-12 00:57:12 -0400

    Import bootstrap css files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
 import App from "./components/";
 import registerServiceWorker from "./registerServiceWorker";
 
