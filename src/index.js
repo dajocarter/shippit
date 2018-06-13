a0d6f7c7714ebfdc0c5a58e5a23e3d23006d5b61
David Carter <dajocarter@gmail.com>
2018-06-13 18:31:53 -0400

    Remove bootstrap css from index.js
    Moved to App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
 import App from "./components/";
 import registerServiceWorker from "./registerServiceWorker";
 
