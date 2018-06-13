c3b27c48fa0e86834c495edb334bd4df69eb0ded
David Carter <dajocarter@gmail.com>
2018-06-13 18:31:30 -0400

    Create main app component
    Includes public/private routing and header

---
+++ src/components/index.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Route, BrowserRouter, Redirect, Switch } from "react-router-dom";
+import { Grid, Row } from "react-bootstrap";
+import Login from "./forms/Login";
+import Register from "./forms/Register";
+import Home from "./Home";
+import Dashboard from "./protected/Dashboard";
+import Header from "./partials/Header";
+import { auth } from "../utils/firebase";
+
+function PrivateRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/dashboard" />
+        )
+      }
+    />
+  );
+}
+
+export default class App extends Component {
+  state = { authed: false, loading: true };
+
+  componentDidMount() {
+    this.stopAuthListener = auth().onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        this.setState({
+          authed: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.stopAuthListener();
+  }
+
+  logout = () => {
+    auth().signOut();
+  };
+
+  render() {
+    return this.state.loading === true ? (
+      <h1>Loading...</h1>
+    ) : (
+      <BrowserRouter>
+        <div>
+          <Header authed={this.state.authed} logout={this.logout} />
+          <Grid>
+            <Row>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <PublicRoute
+                  authed={this.state.authed}
+                  path="/login"
+                  component={Login}
+                />
+                <PublicRoute
+                  authed={this.state.authed}
+                  path="/register"
+                  component={Register}
+                />
+                <PrivateRoute
+                  authed={this.state.authed}
+                  path="/dashboard"
+                  component={Dashboard}
+                />
+                <Route render={() => <h3>404 (page not found)</h3>} />
+              </Switch>
+            </Row>
+          </Grid>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
