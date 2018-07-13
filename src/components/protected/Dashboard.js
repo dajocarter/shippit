c9bcb09635963ec30b63ed30bb141c32429bdccf
David Carter <dajocarter@gmail.com>
2018-07-13 14:06:51 -0700

    Update Dashboard to show Move and Box components

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,12 +1,73 @@
 import React, { Component } from "react";
+import { database } from "../../utils/firebase";
+import { Col } from "react-bootstrap";
+
+import Loading from "../Loading";
+import Box from "../Box";
+import Move from "../Move";
 
 export default class Dashboard extends Component {
+  state = { boxes: [], loading: true };
+
+  componentDidMount() {
+    const db = database().ref(`boxes`);
+    const userBoxesRef = db.child(`${this.props.uid}`);
+    userBoxesRef.on("value", boxesSnapshot => {
+      let boxes = [];
+      if (boxesSnapshot.exists()) {
+        boxesSnapshot.forEach(box => {
+          const boxRef = db.child(`${this.props.uid}/${box.key}`);
+          boxRef
+            .once("value", boxSnapshot => {
+              let boxData = boxSnapshot.val();
+              boxData["key"] = box.key;
+              boxes.push(boxData);
+            })
+            .then(() => {
+              if (boxes.length > 1) {
+                this.setState({
+                  boxes,
+                  loading: false
+                });
+              } else {
+                if (!!boxes[0].name) {
+                  this.setState({
+                    boxes,
+                    loading: false
+                  });
+                } else {
+                  this.setState({
+                    boxes,
+                    loading: false
+                  });
+                }
+              }
+            })
+            .catch(error => console.log(error));
+        });
+      } else {
+        this.setState({
+          loading: false
+        });
+      }
+    });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return (
-      <div>
-        This is the Dashboard. This is a protected route. You can only see this
-        if you're authorized.
-      </div>
+      <Col xs={12}>
+        <Move uid={this.props.uid} />
+        {this.state.boxes.length ? (
+          <div>
+            {this.state.boxes.map(box => <Box key={box.key} box={box} />)}
+          </div>
+        ) : (
+          <Box uid={this.props.uid} history={this.props.history} />
+        )}
+      </Col>
     );
   }
 }
