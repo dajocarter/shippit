a820c94d2285eb8c53a8518b0e7af0119085fdf4
David Carter <dajocarter@gmail.com>
2018-07-17 02:24:18 -0700

    Use controlled inputs in AddItem

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -24,29 +24,42 @@ const SubmitButton = styled(Button)`
   margin-left: 1rem;
 `;
 
+const INITIAL_STATE = { name: "" };
 export default class AddItem extends Component {
-  createItem(event) {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state = INITIAL_STATE;
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
     const item = {
-      name: this.name.value,
+      name: this.state.name,
       box: this.props.boxId
     };
     this.props.addItem(item);
-    this.itemForm.reset();
+    this.setState(INITIAL_STATE);
   }
 
   render() {
     return (
-      <InlineForm
-        inline
-        ref={form => (this.itemForm = form)}
-        onSubmit={e => this.createItem(e)}
-      >
+      <InlineForm inline onSubmit={this.handleSubmit}>
         <InputWrapper controlId="name">
           <Input
             type="text"
+            name="name"
             placeholder="Describe your item"
-            inputRef={name => (this.name = name)}
+            onChange={this.handleChange}
+            value={this.state.name}
           />
         </InputWrapper>
         <SubmitButton type="submit" bsStyle="success">
